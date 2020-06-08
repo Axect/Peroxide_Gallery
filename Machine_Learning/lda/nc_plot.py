a270ece310fd3602e404dd4e7e82ac9becd7b620
Axect <axect@outlook.kr>
2020-06-08 17:13:00 +0900

    LDA 2 - more Fisher

--- Machine_Learning/lda/nc_plot.py
+++ Machine_Learning/lda/nc_plot.py
@@ -2,7 +2,7 @@ from netCDF4 import Dataset
 import matplotlib.pyplot as plt
 
 # Import netCDF file
-ncfile = 'data/least_square.nc'
+ncfile = 'data/lda.nc'
 data = Dataset(ncfile)
 var = data.variables
 
@@ -21,15 +21,21 @@ x1 = var['x1'][:]
 y1 = var['y1'][:]  
 x2 = var['x2'][:]
 y2 = var['y2'][:]
-domain = var['domain'][:]
-fitted = var['fitted'][:]
+d = var['d'][:]
+b1 = var['b1'][:]
+b2 = var['b2'][:]
+bf = var['bf'][:]
 
 # Plot with Legends
-plt.scatter(x1, y1, label=r'Group1')
-plt.scatter(x2, y2, label=r'Group2')
-plt.plot(domain, fitted, label=r'Boundary')
+plt.scatter(x1, y1, label=r'Group1', color='r', alpha=0.5)
+plt.scatter(x2, y2, label=r'Group2', color='b', alpha=0.5)
+plt.plot(d, b1, label=r'Boundary1', color='r', alpha=0.5)
+plt.plot(d, b2, label=r'Boundary2', color='b', alpha=0.5)
+plt.plot(d, bf, label=r'Fisher', color='g', alpha=0.5)
 
 # Other options
+plt.xlim((-15, 15))
+plt.ylim((-15, 15))
 plt.legend(fontsize=12)
 plt.grid()
 plt.savefig("plot/least_square.png", dpi=300)
--- Machine_Learning/lda/src/main.rs
+++ Machine_Learning/lda/src/main.rs
@@ -4,8 +4,8 @@ use peroxide::fuga::*;
 
 fn main() {
     // Generate 2D Random Data
-    let x1 = Normal(3,1).sample(150);
-    let y1 = Normal(1,3).sample(150);
+    let x1 = Normal(1,1).sample(150);
+    let y1 = Normal(0,3).sample(150);
     let x2 = Normal(-3,1).sample(150);
     let y2 = Normal(-1,3).sample(150);
 
@@ -29,23 +29,54 @@ fn main() {
     let w = weight_ls(&z, &t);
     w.print();
 
-    least_square(&w, &c!(3, 1)).print();
-    least_square(&w, &c!(-3, 1)).print();
+    // Test Classification
+    let l1 = least_square(&w, &c!(3, 1));
+    l1.print();
+    classifier(&l1).print();
+    let l2 = least_square(&w, &c!(-3, 1));
+    l2.print();
+    classifier(&l2).print();
     
     // To draw decision boundary
-    //let domain = seq(-5, 5, 0.01);
-    //let fitted = least_square(&w, &domain);
+    let domain = seq(-5, 5, 0.01);
+    let b1 = boundary_1(&w, domain.clone());
+    let b2 = boundary_2(&w, domain.clone());
 
-    //let mut df = DataFrame::with_header(vec!["x1", "y1", "x2", "y2", "domain", "fitted"]);
-    //df["x1"] = x1;
-    //df["y1"] = y1;
-    //df["x2"] = x2;
-    //df["y2"] = y2;
-    //df["domain"] = domain;
-    //df["fitted"] = fitted;
+    // Fisher
+    let m1_x = x1.mean();
+    let m1_y = y1.mean();
+    let m2_x = x2.mean();
+    let m2_y = y2.mean();
+    let m1 = c!(m1_x, m1_y);
+    let m2 = c!(m2_x, m2_y);
+    let m = m1.add_vec(&m2);
+    let mut s1 = zeros(2, 2);
+    s1[(0, 0)] = x1.var();
+    s1[(0, 1)] = cov(&x1, &y1);
+    s1[(1, 0)] = s1[(0, 1)];
+    s1[(1, 1)] = y1.var();
+    let mut s2 = zeros(2, 2);
+    s2[(0, 0)] = x2.var();
+    s2[(0, 1)] = cov(&x2, &y2);
+    s2[(1, 0)] = s2[(0, 1)];
+    s2[(1, 1)] = y2.var();
+    let s = s1 + s2;
+    let mut w_fisher = weight_fisher(&s, m1, m2);
+    w_fisher = w_fisher.normalize(Norm::L2);
+    w_fisher.print();
 
-    //df.print();
-    //df.write_nc("data/least_square.nc").expect("Can't write least_square");
+    let mut df = DataFrame::with_header(vec!["x1", "y1", "x2", "y2", "d", "b1", "b2", "bf"]);
+    df["x1"] = x1;
+    df["y1"] = y1;
+    df["x2"] = x2;
+    df["y2"] = y2;
+    df["d"] = domain.clone();
+    df["b1"] = b1;
+    df["b2"] = b2;
+    df["bf"] = boundary_fisher(&w_fisher, domain, &m);
+
+    df.print();
+    df.write_nc("data/lda.nc").expect("Can't write least_square");
 }
 
 /// Weight
@@ -74,3 +105,46 @@ fn least_square(w: &Matrix, x: &Vec<f64>) -> Vec<f64> {
     y
 }
 
+/// Least square fitting (Vectorized)
+///
+/// # Description
+/// * n: # of domain (usize)
+/// * w: 3 x 2 (Matrix)
+/// * x: 2 x n (Matrix)
+/// * z: 3 x n (Matrix)
+/// * y: 2 x n (Matrix)
+fn least_square_map(w: &Matrix, x: &Matrix) -> Matrix {
+    let mut z = zeros(3, x.col);
+    z.subs_row(1, &vec![1f64; x.col]);
+    z.subs_row(2, &x.row(1));
+    z.subs_row(3, &x.row(2));
+
+    let y = w.t() * z;
+    y
+}
+
+/// Which group?
+fn classifier(y: &Vec<f64>) -> usize {
+    y.arg_max() + 1
+}
+
+/// Decision Boundary 1
+fn boundary_1(w: &Matrix, x: Vec<f64>) -> Vec<f64> {
+    ((x.ox() * (-w[(1, 0)]) - w[(0, 0)] + 0.5f64) / w[(2, 0)]).red()
+}
+
+/// Decision Boundary 2
+fn boundary_2(w: &Matrix, x: Vec<f64>) -> Vec<f64> {
+    ((x.ox() * (-w[(1, 1)]) - w[(0, 1)] + 0.5f64) / w[(2, 1)]).red()
+}
+
+/// Fisher's LDA
+fn weight_fisher(s_w: &Matrix, m1: Vec<f64>, m2: Vec<f64>) -> Vec<f64> {
+    s_w.inv().unwrap() * (m2.ox() - m1.ox()).red()
+} 
+
+/// Boundary of Fisher's LDA
+fn boundary_fisher(w: &Vec<f64>, x: Vec<f64>, m: &Vec<f64>) -> Vec<f64> {
+    //((x.ox() - m[0]) * (-w[0] / w[1]) + m[1]).red()
+    (x.ox() * (-w[0] / w[1])).red()
+}
