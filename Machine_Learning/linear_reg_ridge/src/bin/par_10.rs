02664e2bc57c22f12d5ca11fb4185dfb6b1b7a36
Axect <axect@outlook.kr>
2020-12-22 14:51:07 +0900

    Linear regression version update 0.28.1

--- Machine_Learning/linear_reg_ridge/src/bin/par_10.rs
+++ Machine_Learning/linear_reg_ridge/src/bin/par_10.rs
@@ -1,7 +1,7 @@
 extern crate gaussian;
 extern crate peroxide;
 use gaussian::*;
-use peroxide::*;
+use peroxide::fuga::*;
 
 use std::thread;
 use std::sync::mpsc;
@@ -11,8 +11,7 @@ fn main() {
 
     let sample = gen_sample();
     sample.write_nc("data/data.nc").expect("Can't write data.nc");
-    let x = &sample["x"];
-    let t = &sample["y"];
+    let t: Vec<f64> = sample["y"].to_vec();
 
     let s = 5f64;
 
@@ -25,9 +24,9 @@ fn main() {
             let x_draw = seq(1, 100, 0.1);
             let y_draw = x_draw.fmap(|x| y(s, &w_reg, x));
 
-            let mut df = DataFrame::with_header(vec!["x", "y"]);
-            df["x"] = x_draw;
-            df["y"] = y_draw;
+            let mut df = DataFrame::new(vec![]);
+            df.push("x", Series::new(x_draw));
+            df.push("y", Series::new(y_draw));
             let name = format!("data/par/reg_lam_{}.nc", i);
             df.write_nc(&name).expect("Can't write reg.nc");
             tx_sender.send(i).unwrap();
--- Machine_Learning/linear_reg_ridge/src/lib.rs
+++ Machine_Learning/linear_reg_ridge/src/lib.rs
@@ -1,18 +1,18 @@
 extern crate peroxide;
-use peroxide::*;
+use peroxide::fuga::*;
 
 pub fn f(x: f64) -> f64 {
     (x / 10f64).sin() + (x / 50f64).powi(2)
 }
 
 pub fn gen_sample() -> DataFrame {
-    let mut df = DataFrame::with_header(vec!["x", "y"]);
+    let mut df = DataFrame::new(vec![]);
     let normal = Normal(0, 1);
     let e = normal.sample(100).fmap(|t| 0.2 * t);
     let x = seq(1, 100, 1);
-    let y = x.fmap(f).add(&e);
-    df["x"] = x;
-    df["y"] = y;
+    let y = x.fmap(f).add_v(&e);
+    df.push("x", Series::new(x));
+    df.push("y", Series::new(y));
     df
 }
 
@@ -36,20 +36,20 @@ pub fn design_matrix(s: f64) -> Matrix {
     Matrix::from_index(|i, j| phi(j, s, (i+1) as f64), (100, 101))
 }
 
-pub fn w_mle(s: f64, t: &Vec<f64>) -> Matrix {
+pub fn w_mle(s: f64, t: &Vec<f64>) -> Vec<f64> {
     let phi_mat = design_matrix(s);
-    phi_mat.pseudo_inv().unwrap() * t.to_matrix()
+    phi_mat.pseudo_inv().apply(t)
 }
 
-pub fn y(s: f64, w: &Matrix, x: f64) -> f64 {
+pub fn y(s: f64, w: &Vec<f64>, x: f64) -> f64 {
     let phi = phi_vec(s, x);
-    (w.t() * phi)[(0, 0)]
+    w.dot(&phi)
 }
 
 
-pub fn w_ml_reg(s: f64, lam: f64, t: &Vec<f64>) -> Matrix {
+pub fn w_ml_reg(s: f64, lam: f64, t: &Vec<f64>) -> Vec<f64> {
     let phi_mat = design_matrix(s);
     let lamda_eye = lam * eye(101);
     let pt = phi_mat.t();
-    (&lamda_eye + &(&pt * &phi_mat)).inv().unwrap() * pt * t.to_matrix()
+    ((&lamda_eye + &(&pt * &phi_mat)).inv() * pt).apply(t)
 }
