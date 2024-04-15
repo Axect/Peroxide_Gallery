17501e126eb9d81cb89a528350b4bd58e2620ae9
axect <ax2ct@outlook.com>
2024-04-15 15:01:31 +0900

    Update DCT with v0.37.1

--- Numeric/dct/src/main.rs
+++ Numeric/dct/src/main.rs
@@ -2,7 +2,7 @@ use peroxide::fuga::*;
 use std::f64::consts::PI;
 use rayon::prelude::*;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let t = seq(0.0, 0.5, 0.001);
     let omega = 10f64 * 2f64 * PI;
     let x = t.fmap(|t| {
@@ -14,16 +14,46 @@ fn main() {
 
     let y = dct(&x);
     let x_hat = idct(&y);
+    let omega = (0 .. x.len()).map(|i| i as f64 / 10f64).collect::<Vec<f64>>();
 
-    let mut df = DataFrame::new(vec![]);
-    df.push("t", Series::new(t));
-    df.push("x", Series::new(x));
-    df.push("y", Series::new(y));
-    df.push("x_hat", Series::new(x_hat));
+    //let mut df = DataFrame::new(vec![]);
+    //df.push("t", Series::new(t));
+    //df.push("x", Series::new(x));
+    //df.push("y", Series::new(y));
+    //df.push("x_hat", Series::new(x_hat));
 
-    df.print();
+    //df.print();
 
-    df.write_parquet("dct.parquet", CompressionOptions::Uncompressed).unwrap();
+    //df.write_parquet("dct.parquet", CompressionOptions::Uncompressed).unwrap();
+    let mut plt = Plot2D::new();
+    plt
+        .set_domain(omega)
+        .insert_image(y)
+        .set_legend(vec![r"$y={\rm DCT}(x)$"])
+        .set_xlim((0f64, 10f64))
+        .set_xlabel("Frequency (Hz)")
+        .set_ylabel(r"$y$")
+        .set_style(PlotStyle::Nature)
+        .tight_layout()
+        .set_dpi(600)
+        .set_path("dct_plot.png")
+        .savefig()?;
+
+    let mut plt = Plot2D::new();
+    plt
+        .set_domain(t)
+        .insert_image(x_hat)
+        .insert_image(x)
+        .set_legend(vec![r"$\hat{x} = {\rm iDCT}(y)$", r"$x_{\rm true}$"])
+        .set_xlabel("Time (s)")
+        .set_ylabel(r"$x$")
+        .set_style(PlotStyle::Nature)
+        .tight_layout()
+        .set_dpi(600)
+        .set_path("idct_plot.png")
+        .savefig()?;
+
+    Ok(())
 }
 
 // Type-II Discrete Cosine Transform
