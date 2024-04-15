f7de24b7c62ee8d7f598ecbf3319e0f4a1adf339
axect <ax2ct@outlook.com>
2024-04-15 17:53:52 +0900

    Update lorenz with v0.37.1

--- Numeric/lorenz/README.md
+++ Numeric/lorenz/README.md
@@ -2,30 +2,23 @@
 
 ## Description
 
-Implement Lorenz butterfly with several ODE methods.
+Implement Lorenz butterfly with Peroxide.
+In this project, we use Dormand-Prince 5(4) method to solve the ODE.
 
-* Euler
-* Runge-Kutta 4th order
 
 ## Pre-requisites
 
-* netcdf
-    * libnetcdf
-    * netcdf4 (python)
-* matplotlib
+- Rust
+- matplotlib
+- scineceplots
 
 ## Build Process
 
 ```sh
 # Run
 cargo run --release
-
-# Plot
-python nc_plot.py
 ```
 
 ## Results
 
-![Euler](euler.png)
-
-![RK4](rk4.png)
+![lorenz](lorenz.png)
--- Numeric/lorenz/src/main.rs
+++ Numeric/lorenz/src/main.rs
@@ -1,57 +1,44 @@
-extern crate peroxide;
 use peroxide::fuga::*;
 
+#[allow(unused_variables)]
 fn main() -> Result<(), Box<dyn Error>> {
-    // =========================================
-    //  Declare ODE
-    // =========================================
-    let mut ex_test = ExplicitODE::new(butterfly);
+    let dp45 = DP45::new(1e-4, 0.9, 1e-6, 1e-2, 100);
+    let basic_ode_solver = BasicODESolver::new(dp45);
+    let (_, y_vec) = basic_ode_solver.solve(
+        &Lorenz,
+        (0f64, 100f64),
+        1e-2,
+    )?;
+    let y_mat = py_matrix(y_vec);
+    let y0 = y_mat.col(0);
+    let y2 = y_mat.col(2);
 
-    let init_state: State<f64> = State::new(
-        0.0,
-        vec![10.0, 1.0, 1.0],
-        vec![0.0, 0.0, 0.0],
-    );
-
-    ex_test
-        .set_initial_condition(init_state)
-        .set_method(ExMethod::Euler)
-        .set_step_size(0.01f64)
-        .set_times(10000);
-
-    let mut ex_test2 = ex_test.clone();
-    ex_test2.set_method(ExMethod::RK4);
-
-    // =========================================
-    //  Save results
-    // =========================================
-    let results = ex_test.integrate();
-    let results2 = ex_test2.integrate();
-
-    let mut df_euler = DataFrame::new(vec![]);
-    df_euler.push("t", Series::new(results.col(0)));
-    df_euler.push("x", Series::new(results.col(1)));
-    df_euler.push("y", Series::new(results.col(2)));
-    df_euler.push("z", Series::new(results.col(3)));
-    df_euler.print();
-
-    let mut df_rk4 = DataFrame::new(vec![]);
-    df_rk4.push("t", Series::new(results2.col(0)));
-    df_rk4.push("x", Series::new(results2.col(1)));
-    df_rk4.push("y", Series::new(results2.col(2)));
-    df_rk4.push("z", Series::new(results2.col(3)));
-    df_rk4.print();
-
-    df_euler.write_nc("data/euler.nc")?;
-    df_rk4.write_nc("data/rk4.nc")?;
+    let mut plt = Plot2D::new();
+    plt
+        .set_domain(y0)
+        .insert_image(y2)
+        .set_xlabel(r"$y_0$")
+        .set_ylabel(r"$y_2$")
+        .set_style(PlotStyle::Nature)
+        .tight_layout()
+        .set_dpi(600)
+        .set_path("lorenz.png")
+        .savefig()?;
 
     Ok(())
 }
 
-fn butterfly(st: &mut State<f64>, _: &NoEnv) {
-    let x = &st.value;
-    let dx = &mut st.deriv;
-    dx[0] = 10f64 * (x[1] - x[0]);
-    dx[1] = 28f64 * x[0] - x[1] - x[0] * x[2];
-    dx[2] = -8f64/3f64 * x[2] + x[0] * x[1];
+struct Lorenz;
+
+impl ODEProblem for Lorenz {
+    fn initial_conditions(&self) -> Vec<f64> {
+        vec![10f64, 1f64, 1f64]
+    }
+
+    fn rhs(&self, _t: f64, y: &[f64], dy: &mut [f64]) -> anyhow::Result<()> {
+        dy[0] = 10f64 * (y[1] - y[0]);
+        dy[1] = 28f64 * y[0] - y[1] - y[0] * y[2];
+        dy[2] = -8f64 / 3f64 * y[2] + y[0] * y[1];
+        Ok(())
+    }
 }
