c5aaff558bcde912405efc5f05e14654d188a6da
axect <ax2ct@outlook.com>
2024-04-15 18:19:42 +0900

    Update root finding via ad macro with v0.37.1

--- Numeric/root_finding_macro/src/main.rs
+++ Numeric/root_finding_macro/src/main.rs
@@ -1,8 +1,10 @@
 use peroxide::fuga::*;
 
-fn main() -> Result<(), RootError> {
-    let x = newton(f_ad, 1f64, 100, 1e-15)?;
-    x.print();
+fn main() -> anyhow::Result<()> {
+    let problem = Simple;
+    let newton = NewtonMethod { max_iter: 100, tol: 1e-15 };
+    let x = newton.find(&problem)?;
+    x[0].print();
 
     Ok(())
 }
@@ -10,4 +12,22 @@ fn main() -> Result<(), RootError> {
 #[ad_function] // It generates `f_ad: AD -> AD` automatically
 fn f(x: f64) -> f64 {
     x.exp() - (x + 2f64)
+}
+
+struct Simple;
+
+impl RootFindingProblem<1, 1, f64> for Simple {
+    fn initial_guess(&self) -> f64 {
+        1f64
+    }
+
+    fn function(&self, x: Pt<1>) -> anyhow::Result<Pt<1>> {
+        Ok([f(x[0])])
+    }
+
+    fn derivative(&self, x: Pt<1>) -> anyhow::Result<Jaco<1, 1>> {
+        let ad1 = AD1(x[0], 1f64);
+        let dx = f_ad(ad1).dx();
+        Ok([[dx]])
+    }
 }
